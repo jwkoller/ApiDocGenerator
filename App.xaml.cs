4ec9390e6047bad75c3a4ccb3bed883f1f3e4acb
jwkoller <jonwkoller@gmail.com>
2024-08-07 14:52:49 -0400

    Added title to GUI and reduced window size.

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 850;
+            const int newHeight = 650;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
